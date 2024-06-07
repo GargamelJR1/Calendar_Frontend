256a60553122f1a16de3f48c16474a2298440b04
Direckton <direcktos@gmail.com>
2024-06-07 23:49:14 +0200

    task toggle

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
+import { CommonModule } from '@angular/common';
+import { Task } from '../task'
+
 
 @Component({
   selector: 'app-day',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, CommonModule],
   templateUrl: './day.component.html',
   styleUrl: './day.component.css'
 })
 export class DayComponent {
+  @Input() day!: number;
+  status: boolean = false;
+  state: string = 'viseable';
+  tasks: Task[] = [];
 
+  constructor() {}
+
+  activateTaks() {
+    this.status = !this.status;
+    if(this.status)
+    {
+      this.state = 'visable';
+    }
+    else
+    {
+     this.state = "inviable"; 
+    }
+    this.day += 1;
+  }
 }
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DayComponent } from '../day/day.component';
+
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [],
+  imports: [DayComponent],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
